9ac67dc2218b51f5030727ff604384a971bbcd26
Egor <112505022+YYEg@users.noreply.github.com>
2023-12-31 02:14:45 +0800

    working on main app

---
+++ main/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+
+from main.views import about, index
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('about/', about, name='about'),
+]
--- main/views.py
+++ main/views.py
@@ -1,14 +1,19 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Homepage',
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели Homecare',
     }
 
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About')
+    context = {
+        'title': 'About - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Текст Текст Текст Текст Текст',
+    }
+
+    return render(request, 'main/about.html', context)

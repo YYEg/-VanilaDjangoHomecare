b308131fec96d0ceca0e95f8a62988fab4b2cd96
Egor <112505022+YYEg@users.noreply.github.com>
2023-12-31 03:22:25 +0800

    sqlite db test

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,13 @@
+from enum import unique
+from tokenize import blank_re
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+
+    class Meta:
+        db_table = 'category'

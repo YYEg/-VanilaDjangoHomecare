4516d84a6dc10702a5840d46d94ca13c14120496
Egor <112505022+YYEg@users.noreply.github.com>
2024-01-02 03:13:47 +0800

    true pagination

--- goods/views.py
+++ goods/views.py
@@ -4,7 +4,9 @@ from goods.models import Products
 
 
 # Create your views here.
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+
+    page = request.GET.get('page', 1)
 
     if category_slug == 'all':
         goods = Products.objects.all()
@@ -12,7 +14,7 @@ def catalog(request, category_slug, page=1):
         goods = Products.objects.filter(category__slug=category_slug)
     
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
     context = {
         "title": "Каталог",

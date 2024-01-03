a3dd47109cfbb67ff461e835e58a2514839a56c7
Egor <112505022+YYEg@users.noreply.github.com>
2024-01-03 19:18:34 +0800

    registration, logout

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django.contrib import auth
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 # Create your views here.
 def login(request):
@@ -27,16 +27,39 @@ def login(request):
     return render(request, 'users/login.html', context)
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            # Debugging: Print cleaned_data to check form data
+            cleaned_data = form.cleaned_data
+            print(cleaned_data)
+            
+            # Save the form data
+            form.save()
+
+            # Debugging: Print a message to check if this point is reached
+            print("Form saved successfully")
+
+            user = form.instance
+            auth.login(request, user)
+
+            # Redirect to the login page
+            return HttpResponseRedirect(reverse('main:index'))
+        else:
+            # Debugging: Print form errors
+            print(form.errors)
+    else:
+        form = UserRegistrationForm()
+    
     context = {
-        'title': 'registration'
+        'title': 'registration',
+        'form': form
     }
     return render(request, 'users/registration.html', context)
 
 def logout(request):
-    context = {
-        'title': 'logout'
-    }
-    return render(request, 'users/login.html', context)
+    auth.logout(request)
+    return redirect(reverse('users:login'))
 
 def profile(request):
     context = {

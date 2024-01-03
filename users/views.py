2eea663e6017d17af268df2b9df5e6e935c3b774
Egor <112505022+YYEg@users.noreply.github.com>
2024-01-03 20:50:34 +0800

    login required decorator redirect to target page

--- users/views.py
+++ users/views.py
@@ -18,7 +18,12 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, вы вошли в аккаунт")
-                return HttpResponseRedirect(reverse("main:index"))
+                
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
+                
+                return HttpResponseRedirect(reverse('main:index'))
+                
     else:
         form = UserLoginForm()
 
@@ -55,7 +60,7 @@ def registration(request):
     context = {"title": "registration", "form": form}
     return render(request, "users/registration.html", context)
 
-
+@login_required
 def logout(request):
     auth.logout(request)
     messages.success(request, "Вы вышли из аккаунта")
